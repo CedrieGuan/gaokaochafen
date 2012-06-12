6d6537ca01f28307be5ab184a6e7755900c58f25
Guan.L.Y <mrguanlingyu@gmail.com>
2012-06-12 23:34:45 +0800

    new project

---
+++ src/org/gaokao/GaoKaoActivity.java
@@ -0,0 +1,12 @@
+package org.gaokao;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GaoKaoActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
